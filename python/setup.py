cd44c523e3bf072de3d2b91c97116321239b84c8
Holly Liu <liuhong1.happy@163.com>
2026-04-27 15:53:28 +0800

    publish: 配置pypi发布流程

--- python/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="masrobo_rcs_sdk",
-    version="1.0.0",
-    packages=find_packages(),
-    install_requires=["requests==2.32.3"],
-)
-
